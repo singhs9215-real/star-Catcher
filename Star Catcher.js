09e9787e44447050c86f0d9b01a6a072ce5bddca
singhs9215-real <114897632+singhs9215-real@users.noreply.github.com>
2022-10-03 18:07:17 +1300

    Add files via upload

---
+++ Star Catcher.js
@@ -0,0 +1,35 @@
+const countdown = 10;
+const starArray = ['https://margaret.codewizardshq.com/js_projects_for_kids/star_catcher/star-yellow.png', 'https://margaret.codewizardshq.com/js_projects_for_kids/star_catcher/star-blue.png',  'https://margaret.codewizardshq.com/js_projects_for_kids/star_catcher/star-purple.png'];
+
+const scoreId = document.querySelector("#score");
+let score = 0;
+
+function addScore() {
+  score++;
+  scoreId.innerText = score;
+}
+
+const stars = document.querySelector("#stars");
+stars.addEventListener("click", addScore);
+
+
+function moveStars(){
+  stars.style.top = Math.random()*500+'px';
+  stars.style.left = Math.random()*1300+'px'
+
+  var randomNum = Math.floor(Math.random()*starArray.length);
+  var newStar = starArray[randomNum];
+
+  stars.src = newStar;
+
+}
+
+setInterval(moveStars,1000);
+
+
+function gameOver() {
+	document.querySelector("#gameOver").style.display = 'block';
+	stars.style.display = 'none';
+}
+
+setTimeout(gameOver,10000);
